decc8e8535724e403754c91cdf70efe0eae3f887
thiagofjau <tffjau@gmail.com>
2024-09-12 12:44:54 -0300

    v1

---
+++ src/game.js
@@ -0,0 +1,45 @@
+export const game = {
+    letters: [],
+    error: false,
+    streak: 0,
+    inputedWords: [],
+    newGame: function () {
+        this.letters = [];
+
+        //gerar letras
+
+        const alphabeth = "abcdefghijlmnopqrstuvs";
+
+        this.letters = new Array(3).fill().map(function (letter) {
+            const randomIndex = Math.floor(Math.random() * alphabeth.length);
+            return alphabeth[randomIndex];
+        });
+        this.word = "";
+        this.error = false;
+        this.streak = 0;
+        //criou array com 3 pos
+    },
+    validateWord: async function (word) { //com : pq é mais um atributo do obj game
+        for (let i = 0; i < this.letters.length; i++) {
+            if (word.includes(this.letters[i]) === false) {
+                this.error = true;
+                return false;
+            }
+        }
+
+        if (this.inputedWords.includes(word)) {
+            return false;
+        }
+        const rawData = await fetch(`https://api.dicionario-aberto.net/word/${word}`);
+        const data = await rawData.json();
+
+        if (!data.length) {
+            return false;
+        }
+
+        this.inputedWords.push(word);
+        this.streak++;
+        return true;
+
+    }
+}
--- src/main.js
+++ src/main.js
@@ -1,1 +1,49 @@
 
+import { game } from './game.js';
+
+const newGameButtonElem = document.getElementById("new-game");
+const inputElem = document.getElementById("word");
+const errorElem = document.getElementById("error");
+const streakElem = document.getElementById("streak");
+
+newGameButtonElem.addEventListener('click', function() {
+    game.newGame();
+    const letterElem = document.querySelectorAll(".square");
+
+    for (let i = 0; i < letterElem.length; i++) {
+        letterElem[i].innerHTML = game.letters[i].toUpperCase();
+    }
+ 
+    streakElem.style.visibility = "hidden"; //esconde a streak
+    inputElem.disabled = false;
+    inputElem.value = "";
+    inputElem.focus();
+    errorElem.style.visibility = "hidden";
+
+}) 
+
+document.getElementById("form").addEventListener('submit', async function(event) {
+
+    event.preventDefault();
+    const formData = new FormData(event.target); //formdata pega todas as infos de um form
+    const word = formData.get('word');
+    
+    const isWordValid = await game.validateWord(word.toLocaleLowerCase());
+    if (isWordValid) {
+        streakElem.innerHTML = "Acertos: " + game.streak;
+        streakElem.style.visibility = "visible"; //volta visibilidade
+
+        streakElem.style.fontSize = "3rem";
+        streakElem.style.display = "flex";
+        streakElem.style.justifyContent = "end";
+        streakElem.style.alignItems = "end";
+        inputElem.value = ""; //limpa input para nova palavra/cheat streak
+        inputElem.focus();
+        errorElem.style.visibility = "hidden"; // se errar, aparecer, esconder se acertar
+        // newGameButtonElem.innerHTML = "WIN! | Refresh"; //injeta outro texto no botao
+
+    } else {
+        errorElem.style.visibility = "visible";
+    }
+
+})
